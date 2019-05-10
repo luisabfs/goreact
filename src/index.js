a57ca2d485b378b10e6a0c953605330296bcb0dd
Luísa Barros <qlullyq@gmail.com>
2019-05-10 18:59:20 -0300

    Create first component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-const soma = (a, b) => a + b;
+/* eslint-disable react/no-multi-comp */
+/* eslint-disable no-undef */
+import React, { Component, Fragment } from 'react';
+import { render } from 'react-dom';
 
-alert(soma(6, 4));
+class Button extends Component {
+  render() {
+    return <a href="">Send</a>;
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>Oie</h1>
+        <Button />
+      </Fragment>
+    );
+  }
+}
+
+render(<App />, document.getElementById('app'));
