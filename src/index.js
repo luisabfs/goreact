896aec8b4e6a0800532655d33ff20849bcbac5f4
Luísa Barros <qlullyq@gmail.com>
2019-05-11 02:02:47 -0300

    Default and PropTypes

--- src/index.js
+++ src/index.js
@@ -1,20 +1,48 @@
+/* eslint-disable react/no-typos */
+/* eslint-disable class-methods-use-this */
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable react/no-multi-comp */
 /* eslint-disable no-undef */
 import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
 import { render } from 'react-dom';
 
 class Button extends Component {
   render() {
-    return <a href="">Send</a>;
+    return (
+      <a href="" onClick={this.props.onClick}>
+        {this.props.children}
+      </a>
+    );
   }
 }
 
+Button.defaultProps = {
+  children: 'Salvar',
+};
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  children: PropTypes.string,
+};
+
 class App extends Component {
+  handleClick() {
+    alert('click');
+  }
+
   render() {
     return (
       <Fragment>
         <h1>Oie</h1>
-        <Button />
+        <Button
+          onClick={() => {
+            alert('Button 1');
+          }}
+        />
+        <Button onClick={this.handleClick}>Enviar</Button>
       </Fragment>
     );
   }
